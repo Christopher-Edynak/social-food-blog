674c75ebb8a505355bc9a6d1a8c276bf18786e28
[Christopher Edynak] <[christopher.edynak@outlook.com]>
2022-08-19 15:44:01 -0400

    implemented JS function to display label menu

--- frontend/dashboard.php
+++ frontend/dashboard.php
@@ -243,6 +243,7 @@
 				<?php $dashObj->getAllPosts('Post','',$blog->blogID); ?>
  			</div>
  			<!-- JS FILES -->
+      <script type="text/javascript" src="<?php echo BASE_URL; ?>frontend/assets/js/labelMenu.js"></script>
 		</div>
 		<!--MAIN-Right-inner-DIV-ENDS-HERE-->
 		</div>
