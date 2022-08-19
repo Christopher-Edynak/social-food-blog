1870c3a066c38eafb6c0de60af60d3ba484a4565
[Christopher Edynak] <[christopher.edynak@outlook.com]>
2022-08-19 16:13:22 -0400

    implemented JS function to display add a new label box

--- frontend/assets/js/labelMenu.js
+++ frontend/assets/js/labelMenu.js
@@ -6,6 +6,35 @@ button.addEventListener("click", function(event){
     labelMenu.classList.toggle("display");
     var newLabel = document.querySelector("#newLabel");
 
+
+    newLabel.addEventListener("click", function(event){
+      var checkBox = document.querySelectorAll(".postCheckBox");
+      var array = new Array();
+      
+      checkBox.forEach(function(el){
+        if(el.checked){
+                  array.push(el.value);
+        }
+      });
+
+      if(array.length > 0){
+        var newLabelValue = prompt("Enter the new label");
+
+        if(newLabelValue != null || newLabelValue != ''){
+          var regex  = /^[a-z0-9]+/i;
+          var label  = newLabelValue.match(regex);
+
+
+				  if(label !== null){
+					//Ajax request
+          alert('its working');       
+          }
+        }
+      }else{
+        alert("No posts are selected!");
+        location.reload(true);
+      }
+    });
     
     document.onclick = function(e){
       e.stopPropagation();
