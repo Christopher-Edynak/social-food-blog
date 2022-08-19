5ce666de8d48e8e0f765d0e5f9d4e88acfdf614f
[Christopher Edynak] <[christopher.edynak@outlook.com]>
2022-08-19 15:21:32 -0400

    implemented a method to display post labels

--- backend/classes/Dashboard.php
+++ backend/classes/Dashboard.php
@@ -54,7 +54,7 @@
                                         </div>
                                         <div class="pl-h-lf-link">
                                             <ul>
-                                            {Labels}
+                                            '.$this->getPostLabels($post->postID, $post->blogID).'
                                             </ul>
                                         </div>
                                     </div>
@@ -105,6 +105,21 @@
                 </div>
                 </div>';
           }       
+        }
+
+        public function getPostLabels($postID, $blogID){
+          $stmt = $this->db->prepare("SELECT * FROM `labels` WHERE `postID` = :postID AND `blogID` = :blogID");
+          $stmt->bindParam(":postID", $postID, PDO::PARAM_INT);
+          $stmt->bindParam(":blogID", $blogID, PDO::PARAM_INT);
+          $stmt->execute();
+          $labels = $stmt->fetchAll(PDO::FETCH_OBJ);
+          $i = 1;
+          $return = '';
+          foreach($labels as $label){
+                    $return .= '<li><a href="#">'.$label->labelName.'</a></li>'.(($i < count($labels)) ? ', ' : '');
+                    $i++;
+          }
+          return $return;
         }
     }
             
