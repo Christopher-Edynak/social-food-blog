c4d427cb7eaca1746511a6e67235835596668d28
[Christopher Edynak] <[christopher.edynak@outlook.com]>
2022-08-23 15:02:03 -0400

    implemented method and Ajax request to revert posts to draft

--- backend/ajax/draftPosts.php
+++ backend/ajax/draftPosts.php
@@ -3,8 +3,8 @@
 
 	if($_SERVER['REQUEST_METHOD'] === "POST"){
 		if(isset($_POST['postIDs'])){
-			$postIDs = json_decode($_POST['postIDs']);
-			$blogIDs = json_decode($_POST['blogIDs']);
+			$postIDs   = json_decode($_POST['postIDs']);
+			$blogIDs   = json_decode($_POST['blogIDs']);
 
 			if(!empty($postIDs) && !empty($blogIDs)){
 				foreach($postIDs as $postID){
@@ -14,11 +14,11 @@
 
 						if($blog){
 							if($blog->role === "Admin" OR $blog->userID === $post->authorID){
-                                //update post
-                                $userObj->update('posts', ['postStatus' => 'published'], ['postID' => $post->postID, 'blogID' => $blog->blogID]);
+								//update post
+						 		$userObj->update('posts', ['postStatus' => 'draft'], ['postID' => $post->postID, 'blogID' => $blog->blogID]);
 
 							}else{
-								echo 'You don\'t have the rights to perform this action!';
+								echo 'You don\'t have the rights to preform this action!';
 								break;
 							}
 						}else{
@@ -29,5 +29,5 @@
 				}
 			}
 		}
-  }
+    }
 ?>
